b647d63db7b253777436a2f9de7e48b3945b7a70
adurso0747 <adurso0747@students.mc3.edu>
2016-04-23 19:17:48 -0400

    Added comments

--- Emotion-Tracker.py
+++ Emotion-Tracker.py
@@ -3,76 +3,136 @@ Big thanks to @momogary and @liy9393 for the code which this project was based o
 Using microsoft emotion free api. Get an api key here:  https://www.microsoft.com/cognitive-services/en-us/emotion-api
 '''
 import cv2
+import operator
 from cv2 import cv
 import httplib, urllib, base64
+
 def faceDetection(image):
-    
+
+    #Sets up request from Microsoft API
     headers = {
         # Request headers
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
     }
-    
+
+    #Opens webcam.jpg in reading binary form ('rb')
     data = open('webcam.jpg', 'rb').read()
     
     try:
+        '''
+        Assigns conn to httplib HTTPSConnection method
+        which connects to projectoxford api
+        '''
         conn = httplib.HTTPSConnection('api.projectoxford.ai')
 
+        '''
+        Uses urllib.request method to form a POST request from
+        the Microsoft emotion API. Passes webcam.jpg and
+        the api key to be processed
+        '''
         conn.request("POST", "/emotion/v1.0/recognize" , data , headers)
 
+        '''
+        After a request is sent getresponse gets the response from the server.
+        Returns an HTTPResponse instance and assigns it to variable response
+        '''
         response = conn.getresponse()
 
+        #Reads https response and assigns string to data variable
         data = response.read()
-        #print(data)
-        data = eval(data)  # type list
-        #print type(data)
-        #print data
+
+        #Evaluates data so it can be processed by drawBBox function
+        data = eval(data) 
+
+        #Closes https connection
         conn.close()
+
+    #Allows for an exception if https connection fails
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
     return data
 
+#Function to draw box and text around image
 def drawBBox(response , image):
-    #from PIL import Image
+
     '''
+    Example response:
+    
     response = [
-        {"faceRectangle":{"height":113,"left":624,"top":109,"width":113},"scores":{"anger":3.094097E-06,"contempt":5.47389654E-08,"disgust":1.40311486E-05,"fear":2.1335E-09,"happiness":0.9999826,"neutral":1.21930185E-07,"sadness":2.03058121E-08,"surprise":7.509437E-08}},
-        {"faceRectangle":{"height":112,"left":230,"top":145,"width":112},"scores":{"anger":1.62075583E-12,"contempt":8.48001E-13,"disgust":4.43286114E-13,"fear":1.20183927E-15,"happiness":1.0,"neutral":5.69783977E-12,"sadness":2.131319E-14,"surprise":3.8522345E-12}},
-        {"faceRectangle":{"height":103,"left":525,"top":184,"width":103},"scores":{"anger":5.1391353E-11,"contempt":1.26054183E-13,"disgust":6.300444E-11,"fear":2.00125181E-12,"happiness":1.0,"neutral":1.11211517E-12,"sadness":5.692164E-14,"surprise":6.25029639E-10}},
-        {"faceRectangle":{"height":100,"left":348,"top":186,"width":100},"scores":{"anger":1.46614942E-10,"contempt":6.196312E-10,"disgust":3.89794273E-11,"fear":1.38707434E-10,"happiness":1.0,"neutral":4.158724E-09,"sadness":5.360124E-12,"surprise":1.9608418E-08}},
-        {"faceRectangle":{"height":98,"left":121,"top":205,"width":98},"scores":{"anger":8.750456E-07,"contempt":1.87719866E-06,"disgust":6.564448E-07,"fear":4.57608245E-08,"happiness":0.999996245,"neutral":2.52363776E-07,"sadness":3.497753E-08,"surprise":3.27670975E-08}}
-    ]
+        {"faceRectangle":{"height":XXX,"left":XXX,"top":XXX,"width":XXX},
+         "scores":{"anger":X.XXXE-XX,"contempt":X.XXXXE-XX,"disgust":X.XXXXXE-XX,"fear":X.XXXXE-XX,
+         "happiness":X.XXXXXE-XX,"neutral":X.XXXXE-XX,"sadness":X.XXXXE-XX,"surprise":X.XXXXE-XX}},
+      
     '''
 
-    import cv2
-    import operator
-
+    #Sets color to RGB value of blue
     color = (255,0,0)
+
+    #Sets font to hershey simplex
     font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    '''
+    sortField here is assigned to
+    operator.itemgetter to be used to return a tuple of lookup values.
+    '''
     sortField = operator.itemgetter(1)
+
+    #Sets textGap to 15
     textGap = 15
 
+    #Uses OpenCV to assign webcam.jpg to im variable
     im = cv2.imread('webcam.jpg')
+    
     for rect in response:
+        #Assigns height variable to https response height
         height = rect["faceRectangle"]["height"]
+        
+        #Assigns width variable to https response width
         width = rect["faceRectangle"]["width"]
+
+        #Assigns start_point to top left coordinate of rectangle in https response
         start_point = (rect["faceRectangle"]["left"],rect["faceRectangle"]["top"])
+
+        #Assigns end_point to bottom right coordinate of rectangle in https response
         end_point = ( start_point[0] + width , start_point[1] + height )
+
+        '''
+        Draws a rectangle around picture using OpenCV rectangle method.
+        Uses webcam.jpg as its basis, then draws a rectangle using start and end points
+        gathered from the https response. Assigns this rectangle color which in RGB values
+        is blue. Assigns rectangle a thickness of 2.
+        '''
         cv2.rectangle(im, start_point, end_point, color, 2)
 
+        #Sets emotions equal to values in the emotion scores from https response
         emotions = rect["scores"]
+
+        #Iterates through emotions to create a tuple of lookup values
         emotions = sorted(emotions.iteritems(),key=sortField,reverse=True)
 
+        #Loop to sort through tuple of emotion values
         cnt = 1
         for tup in emotions:
+            
+            #If tuple is less than .1 it is skipped over
             if tup[1] < 0.1:
                 continue
+            '''
+            Else the tuple value for the percentage of the emotion is put
+            at the bottom right hand corner of the box in red
+            '''
             cv2.putText(im, "%s %.2f" % ( tup[0] ,tup[1]) ,( start_point[0], end_point[1] + textGap * cnt ), font, 0.4,(0,0,255),1)
             cnt += 1
 
+    #Creates a window named face detection
     cv2.namedWindow("face detection")
+
+    #Shows webcam.jpg 
     cv2.imshow("face detection", im)
+
+    #Waits for any key to be pressed to destroy the window created by the drawBBox function
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
@@ -85,11 +145,11 @@ class Picture():
     #Creates a variable called vc which holds the Video capture of the first detected device. This is noted by the (0).
     vc = cv2.VideoCapture(0)
     
-    """
+    '''
     try to get the first frame
     if video capture is opened rval is set to true and a new mat variable named frame is set to
     vc.read
-    """
+    '''
     
     if vc.isOpened(): 
         rval, frame = vc.read()
@@ -101,22 +161,22 @@ class Picture():
    
     while rval:
         
-        """
+        '''
         while rval is true:
         Use opencv method imshow to show the frame in a box called preview
-        """
+        '''
         cv2.imshow("preview", frame)
         
         #reads frame by frame through the video capture while a webcam is open
         rval, frame = vc.read()
         
-        """
+        '''
         Writing waitKey(40) will make the program wait for 40 millisecond.
         In this duration it will also check whether any key is pressed or not .
         If any key is pressed than this will return the ASCII value of that pressed key. 
         As explained in the OpenCV documentation, HighGui (imshow() is a function of HighGui)
         needs a call of waitKey regularly, in order to process its event loop.
-        """
+        '''
         key = cv2.waitKey(40)
         
         #ASCII value of esc is 27. Pressing it here will exit the program. 
